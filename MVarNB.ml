660548c923f75755f2ffcc872fec366a5f183b39
Deepali <deepaliande.1@gmail.com>
2022-03-15 13:01:29 +0530

    Non-blocking implementation

---
+++ MVarNB.ml
@@ -0,0 +1,101 @@
+open EffectHandlers
+
+(* module Fun_queue = Fun_queue.t *)
+
+type 'a mv_state =
+  | Full  of 'a * ('a * unit Sched.resumer) Fun_queue.t
+  | Empty of 'a Sched.resumer Fun_queue.t
+
+(* type 'a t = 'a mv_state ref Atomic.t *)
+
+type 'a t = 'a mv_state Atomic.t 
+
+(* let create_empty () = ref (Empty (Ws_deque.create ())) *)
+let create_empty () = Atomic.make (Empty (Fun_queue.empty))
+(* let create v = ref (Full (v, Ws_deque.create ())) *)
+let create v = Atomic.make (Full (v, Fun_queue.empty))
+
+
+let put v mv =
+  let old_contents = Atomic.get (mv) in
+  match old_contents with
+  | Full (v', q) -> let ret1 = perform (Sched.Suspend (fun r -> 
+                                            let newQueue = Fun_queue.push q (v,r) in
+                                            let new_contents = Full (v', newQueue) in
+                                            let ret = Atomic.compare_and_set mv old_contents new_contents in
+                                            if ret then true
+                                            else false
+                                          )
+                            ) in
+                    if ret1 then
+                      Printf.printf "Suspend True"
+                    else Printf.printf "Suspend False Retry" 
+  | Empty q -> if Fun_queue.length q = 0 then 
+                  begin
+                    let new_contents = Full (v, Fun_queue.empty) in
+                    let ret = Atomic.compare_and_set mv old_contents new_contents in 
+                    if ret then 
+                      Printf.printf "Empty  Empty True"
+                    else Printf.printf "Empty Empty False Retry"   
+                  end            
+              else
+                  (* begin *)
+                    let _ = 
+                          (match Fun_queue.pop q with
+                          | None -> ()
+                          | Some (x, newQueue) -> (let resume = x in
+                                                  let new_contents = Empty newQueue in
+                                                  let ret = Atomic.compare_and_set mv old_contents new_contents in 
+                                                  if ret then
+                                                    begin
+                                                    resume v;
+                                                    Printf.printf "Empty resume true"
+                                                    end
+                                                  else
+                                                    Printf.printf "Empty resume false Retry") )
+                                                    in ()
+                                                        
+
+
+(* TODO -> Check if v is to be returned only on true cases or false cases too!!! *)
+let take mv =
+  let old_contents = Atomic.get mv in 
+  match old_contents with
+  | Empty q -> let ret = perform (Sched.Suspend (fun r -> 
+                                            let newQueue = Fun_queue.push q r in
+                                            let new_contents = Empty newQueue in
+                                            let ret = Atomic.compare_and_set mv old_contents new_contents in
+                                            if ret then true
+                                            else false
+                                          )
+                            ) in
+                    if ret then
+                      Printf.printf "Take Suspend True"
+                    else Printf.printf "Take Suspend False Retry" 
+  | Full (v, q) ->
+                if Fun_queue.length q = 0 then
+                  begin
+                    let new_contents = Empty Fun_queue.empty in
+                    let ret = Atomic.compare_and_set mv old_contents new_contents in 
+                    (if ret then 
+                      Printf.printf "Take Full  Empty True"
+                    else Printf.printf "Take Full Empty False Retry" 
+                    );
+                    v
+                  end 
+                else
+                      let _ = 
+                          (match Fun_queue.pop q with
+                          | None -> ()
+                          | Some ((v', resume), newQueue) -> 
+                                                  (let new_contents = Full (v', newQueue) in
+                                                  let ret = Atomic.compare_and_set mv old_contents new_contents in 
+                                                  if ret then
+                                                    begin
+                                                    resume ();
+                                                    Printf.printf "Empty resume true";
+                                                    v
+                                                    end
+                                                  else
+                                                    Printf.printf "Empty resume false Retry";v) ) in ()
+                
---
+++ MVarNB.mli
@@ -0,0 +1,7 @@
+(* Taken from https://github.com/kayceesrk/code-snippets/blob/master/scheduler_parateric_mvar/MVar.mli *)
+type 'a t
+val create : 'a -> 'a t
+val create_empty : unit -> 'a t
+val put : 'a -> 'a t -> unit
+val take : 'a t -> 'a
+(* val check : unit -> unit *)
